d7b932c544d6762564cef302b625d4115848f715
jl iligan <jliligan23@gmail.com>
2025-07-03 09:31:35 +0000

    i want the employee to have a 15 days leave anually

--- src/app/leave/page.tsx
+++ src/app/leave/page.tsx
@@ -79,7 +79,7 @@ const leaveRequests = [
 ];
 
 const summaryCards = [
-    { title: "Annual Leave Balance", value: "12 Days", icon: CalendarDays },
+    { title: "Annual Leave Balance", value: "15 Days", icon: CalendarDays },
     { title: "Sick Leave Balance", value: "8 Days", icon: CalendarDays },
     { title: "Pending Requests", value: "2", icon: ClipboardCheck },
 ];
